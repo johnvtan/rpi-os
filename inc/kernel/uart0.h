e985545f6b5b484900d71a47c71e517f1191599a
johnvtan <john.v.tan@gmail.com>
2018-05-31 11:11:13 -0400

    changed register definitions for uart to include union with uint32_t

--- inc/kernel/uart0.h
+++ inc/kernel/uart0.h
@@ -6,119 +6,185 @@
 #define UART0_BASE_ADDR 0x3F201000U
 
 typedef struct {
-    uint32_t data        :8;
-    uint32_t frame_err   :1;
-    uint32_t parity_err  :1;
-    uint32_t break_err   :1;
-    uint32_t overrun_err :1;
-    uint32_t reserved1   :19;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t data        :8;
+            uint32_t frame_err   :1;
+            uint32_t parity_err  :1;
+            uint32_t break_err   :1;
+            uint32_t overrun_err :1;
+            uint32_t reserved1   :19;
+        }
 } UART0_DR;
 
 typedef struct {
-    uint32_t frame_err   :1;
-    uint32_t parity_err  :1;
-    uint32_t break_err   :1;
-    uint32_t overrun_err :1;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t frame_err   :1;
+            uint32_t parity_err  :1;
+            uint32_t break_err   :1;
+            uint32_t overrun_err :1;
+        }
+    }
+    
 } UART0_RSRECR;
 
 typedef struct {
-    uint32_t clear_to_send :1;
-    uint32_t reserved1     :2;
-    uint32_t busy          :1;
-    uint32_t rx_empty      :1;
-    uint32_t tx_full       :1;
-    uint32_t rx_full       :1;
-    uint32_t tx_empty      :1;
-    uint32_t reserved2     :24;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t clear_to_send :1;
+            uint32_t reserved1     :2;
+            uint32_t busy          :1;
+            uint32_t rx_empty      :1;
+            uint32_t tx_full       :1;
+            uint32_t rx_full       :1;
+            uint32_t tx_empty      :1;
+            uint32_t reserved2     :24;
+        }
+    }
 } UART0_FR;
 
 typedef struct {
-    uint32_t value      :16;
-    uint32_t reserved1  :16;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t data       :16;
+            uint32_t reserved1  :16;
+        }
+    }
 } UART0_IBRD;
 
 typedef struct {
-    uint32_t value    :6;
-    uint32_t reserved :26;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t data     :6;
+            uint32_t reserved :26;
+        }
+    }
 } UART0_FBRD;
 
 typedef struct {
-    uint32_t brk          :1;
-    uint32_t parity_en    :1;
-    uint32_t even_parity  :1;
-    uint32_t stop_2       :1;
-    uint32_t fifo_en      :1;
-    uint32_t word_len     :2;
-    uint32_t stick_parity :1;
-    uint32_t reserved1    :24;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t brk          :1;
+            uint32_t parity_en    :1;
+            uint32_t even_parity  :1;
+            uint32_t stop_2       :1;
+            uint32_t fifo_en      :1;
+            uint32_t word_len     :2;
+            uint32_t stick_parity :1;
+            uint32_t reserved1    :24;
+        }
+    }
 } UART0_LCRH;
 
 typedef struct {
-    uint32_t uart_en    :1;
-    uint32_t reserved1  :6;
-    uint32_t loopback_en :1;
-    uint32_t tx_en      :1;
-    uint32_t rx_en      :1;
-    uint32_t dtr        :1;
-    uint32_t rts        :1;
-    uint32_t out1       :1;
-    uint32_t out2       :1;
-    uint32_t rts_en     :1;
-    uint32_t cts        :1;
-    uint32_t reserved2  :16;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t uart_en    :1;
+            uint32_t reserved1  :6;
+            uint32_t loopback_en :1;
+            uint32_t tx_en      :1;
+            uint32_t rx_en      :1;
+            uint32_t dtr        :1;
+            uint32_t rts        :1;
+            uint32_t out1       :1;
+            uint32_t out2       :1;
+            uint32_t rts_en     :1;
+            uint32_t cts        :1;
+            uint32_t reserved2  :16;
+        }
+    }
 } UART0_CR;
 
 typedef struct {
-    uint32_t tx_ifl_sel :3;
-    uint32_t rx_ifl_sel :3;
-    uint32_t reserved1  :26;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t tx_ifl_sel :3;
+            uint32_t rx_ifl_sel :3;
+            uint32_t reserved1  :26;
+        }
+    }
 } UART0_IFLS;
 
 typedef struct {
-    uint32_t ri  :1;
-    uint32_t cts :1;
-    uint32_t dcd :1;
-    uint32_t dsr :1;
-    uint32_t rx  :1;
-    uint32_t tx  :1;
-    uint32_t rt  :1;
-    uint32_t fe  :1;
-    uint32_t pe  :1;
-    uint32_t be  :1;
-    uint32_t oe  :1;
-    uint32_t reserved1 :21;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t ri  :1;
+            uint32_t cts :1;
+            uint32_t dcd :1;
+            uint32_t dsr :1;
+            uint32_t rx  :1;
+            uint32_t tx  :1;
+            uint32_t rt  :1;
+            uint32_t fe  :1;
+            uint32_t pe  :1;
+            uint32_t be  :1;
+            uint32_t oe  :1;
+            uint32_t reserved1 :21;
+        }
+    }
 } UART0_INT_REG;
 
 typedef struct {
-    uint32_t itcr0  :1;
-    uint32_t itcr1  :1;
-    uint32_t reserved1 :30;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t itcr0  :1;
+            uint32_t itcr1  :1;
+            uint32_t reserved1 :30;
+        }
+    }
 } UART0_ITCR;
 
 typedef struct {
-    uint32_t itip0     :1;
-    uint32_t reserved1 :2;
-    uint32_t itip3     :1;
-    uint32_t reserved2 :28;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t itip0     :1;
+            uint32_t reserved1 :2;
+            uint32_t itip3     :1;
+            uint32_t reserved2 :28;
+        }
+    }
 } UART0_ITIP;
 
 typedef struct {
-    uint32_t itip0     :1;
-    uint32_t reserved1 :2;
-    uint32_t itip3     :1;
-    uint32_t reserved2 :2;
-    uint32_t itip6     :1;
-    uint32_t itop7     :1;
-    uint32_t itop8     :1;
-    uint32_t itop9     :1;
-    uint32_t itop10    :1;
-    uint32_t itop11    :1;
-    uint32_t reserved3 :20;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t itip0     :1;
+            uint32_t reserved1 :2;
+            uint32_t itip3     :1;
+            uint32_t reserved2 :2;
+            uint32_t itip6     :1;
+            uint32_t itop7     :1;
+            uint32_t itop8     :1;
+            uint32_t itop9     :1;
+            uint32_t itop10    :1;
+            uint32_t itop11    :1;
+            uint32_t reserved3 :20;           
+        }
+    }
+
 } UART0_ITOP;
 
 typedef struct {
-    uint32_t tdr10_0    :11;
-    uint32_t reserved   :21;
+    union {
+        uint32_t val;
+        struct {
+            uint32_t tdr10_0    :11;
+            uint32_t reserved   :21;
+        }
+    }
 } UART0_TDR;
 
 struct UART0 {
